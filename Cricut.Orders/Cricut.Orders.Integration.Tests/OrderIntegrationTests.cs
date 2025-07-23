0aee98f081ef2844ccc3800642750343d1e7ac41
Nicole Nelson <n.laherty.nelson@gmail.com>
2025-07-22 20:03:52 -0600

    Add test for Get all Orders for customer

--- Cricut.Orders/Cricut.Orders.Integration.Tests/OrderIntegrationTests.cs
+++ Cricut.Orders/Cricut.Orders.Integration.Tests/OrderIntegrationTests.cs
@@ -40,6 +40,23 @@ namespace Cricut.Orders.Integration.Tests
             }
         }
 
+        [DataTestMethod]
+        [DataRow(12345, 5)]
+        [DataRow(54321, 5)]
+        [DataRow(0, 0)]
+        public async Task GetAllOrdersForCustomer_Num_Of_Orders(int customerId, int numOfOrders)
+        {
+            var client = OrdersApiTestClientFactory.CreateTestClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "v1/orders?customerId=" + customerId);
+
+            var response = await client.SendAsync(request);
+            response.IsSuccessStatusCode.Should().BeTrue();
+            var orders = await response.Content.ReadFromJsonAsync<OrderViewModel[]>();
+
+            orders.Should().HaveCount(numOfOrders);
+        }
+
         private NewOrderViewModel CreateOrderWithItems(int numberOfLineItems, int quantityOfEachItem, double priceOfEachItem)
         {
             var orderItems = new AutoFaker<OrderItemViewModel>()
